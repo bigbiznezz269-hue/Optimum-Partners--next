73b81550ce39960db4fa00ccae4b4ca815ed46f6
bigbiznezz269-hue <bigbiznezz269@gmail.com>
2025-09-18 11:56:00 -0400

    Delete pages.index.js

--- pages.index.js
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <div style={{ textAlign: "center", marginTop: "50px" }}>
-      <h1>Welcome to Optimum Partners</h1>
-      <p>This is your homepage powered by Next.js.</p>
-    </div>
-  );
-}
